6a36fd33cb68465e91fbaf977cf893d1153d2530
abou <abou.mlanao@gmail.com>
2022-09-05 20:02:54 +0200

    service getAll()

--- src/app/shared/services/grammagik/grammagik.service.ts
+++ src/app/shared/services/grammagik/grammagik.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { GramMagik } from 'src/app/models/gram-magik';
+import { Profil } from 'src/app/models/profil';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,36 @@ export class GrammagikService {
   constructor() { }
 
   getAll(){
+    const allGramMagics : GramMagik[] = []
+  
     
-  }
+      let gramMagic = new GramMagik();
+      gramMagic.urlImg = '../assets/img/02-cocktail-cucumber-rose-soda.gif';
+      gramMagic.titre = 'Pétillance exigé';
+
+      gramMagic.profil = new Profil();
+      gramMagic.profil.nom = 'Larry';
+
+      allGramMagics.push(gramMagic)
+
+ 
+
+  // 2eme facon de faire
+  allGramMagics.push({
+    urlImg:'../assets/img/05-soupoudrage-cake.gif',
+    titre:'Perlimpinpin saupoudrage de perlimpinpinexigé',
+    profil:{nom: 'Poter', gramMagik: []}
+  },
+  {
+    urlImg:'../assets/img/05-soupoudrage-cake.gif',
+    titre:'Perlimpinpin saupoudrage de perlimpinpinexigé',
+    profil:{nom: 'Poter', gramMagik: []}
+  })
+
+
+  return allGramMagics;  // peut importe la facon ne pas oublier de return
+
+
+}
+
 }
