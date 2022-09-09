25622f9fe59afad89e1ab1198764cda7317334f0
abou <abou.mlanao@gmail.com>
2022-09-09 12:36:15 +0200

    git environement.prod url mock

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 
   apis:{
     grammagik:{
-      url: '<<à renseigner>>'
-      // url: 'https://c33fe049-4bc3-45bd-a0f5-466263f1aaa7.mock.pstmn.io/grammagik'
+      // url: '<<à renseigner>>'
+      url: 'https://c33fe049-4bc3-45bd-a0f5-466263f1aaa7.mock.pstmn.io/grammagik'
     }
   },
 };
